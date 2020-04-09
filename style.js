8286c5a1e9aedadf992f4db1e64f037ca8e599ad
Gabriel Zsigmond <gaazsig@gmail.com>
2020-04-09 18:02:31 -0300

    created navbar IS NOT responsive yet

--- style.js
+++ style.js
@@ -1,3 +1,20 @@
+window.onscroll = () => {
+  stickNavbar();
+};
+
+const stickNavbar = () => {
+  if (document.getElementById("navbar-sticky")) {
+    let navbar = document.getElementById("navbar-sticky");
+    let sticky = navbar.offsetTop;
+    if (window.pageYOffset > sticky) {
+      navbar.classList.add("g-sticky");
+    } else {
+      navbar.classList.remove("g-sticky");
+    }
+  }
+};
+
+
 const openModal = (event) => {
   event.nextSibling.classList.remove("g-fade-out");
   document.querySelector("body").classList.add("g-hide-overflow");
